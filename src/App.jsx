6f2e80a8051ef7c2bfcd90138251936b888b63f5
sarayminar <sarayminar@gmail.com>
2025-03-20 14:24:00 +0100

    refactor: reorganize folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import './App.css'
-import Button from './components/ui/Button/Button'
-import IconContainer from './components/ui/IconContainer/IconContainer'
-import Input from './components/ui/Input/Input'
-import Product from './components/ui/Product/Product'
+import "./App.css";
+import Button from "./components/ui/Button/Button";
+import IconContainer from "./components/ui/IconContainer/IconContainer";
+import Input from "./components/ui/Input/Input";
+import ProductCard from "./components/ui/ProductCard/ProductCard";
+
 
 function App() {
-
   return (
     <>
-      <Button onClick="" variation='light' type='button' text="Contactar" />
-      <IconContainer variation='apple'/>
-      <IconContainer variation='like'/>
-      <Input variation='product-input'/>
-      <Product/>
+      <Button onClick="" variation="light" type="button" text="Contactar" />
+      <IconContainer variation="apple" />
+      <IconContainer variation="like" />
+      <Input variation="product-input" />
+      <ProductCard
+        image="https://via.placeholder.com/250"
+        name="Ejemplo de Producto"
+        rating={4}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
