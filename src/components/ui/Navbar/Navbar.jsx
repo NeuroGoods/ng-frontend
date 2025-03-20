6e10e167ce4b36d8ef8fca5b529471d9cf59eb2f
sarayminar <sarayminar@gmail.com>
2025-03-20 17:20:57 +0100

    feat(navbar): setting up react router

--- src/components/ui/Navbar/Navbar.jsx
+++ src/components/ui/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import IconContainer from "../IconContainer/IconContainer"
 import styles from './Navbar.module.css';
+import {NavLink, Navigate} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-        <IconContainer variation='envelope'/>
-        <IconContainer variation='bell'/>
-        <IconContainer variation='plus'/>
-        <IconContainer variation='favorites'/>
+      <NavLink to='/'><IconContainer variation='envelope'/> </NavLink >
+      <NavLink  to='/'><IconContainer variation='bell'/></NavLink >
+      <NavLink to='/publish-product'><IconContainer variation='plus'/></NavLink >
+      <NavLink to='/favorites'><IconContainer variation='favorites'/></NavLink >
     </div>
   )
 }
