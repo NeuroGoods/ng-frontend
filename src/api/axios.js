5321d9d3961863f52e6f4cfa38eb07fb99220bfe
issamchlf <issamchellaf734@gmail.com>
2025-03-20 14:12:48 +0100

    feat: fetch products from API and update App component

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-export const axiosclient = axios.create({
-    baseURL: 'http://localhost:8000',
-    withCredentials: true,
-    withXSRFToken: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
+
+export const axiosClient = axios.create({
+  baseURL: 'http://localhost:8000', // Ensure it matches Laravel's base URL
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
 });
