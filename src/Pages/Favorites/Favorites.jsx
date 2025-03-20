bd1bd17f9d669b0055686351b926b6b816328f3c
sarayminar <sarayminar@gmail.com>
2025-03-20 22:53:23 +0100

    feat(favorites): adding delete

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ContainerProductDetail from "../../components/ui/ContainerProductDetail/ContainerProductDetail";
 import ImageContainer from "../../components/ui/ImageContainer/ImageContainer";
+import { FaTrash } from "react-icons/fa"; // Importamos el ícono de la papelera
 import "./favorites.css"; // Importamos los estilos
 
 const Favorites = () => {
-  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem("favorites")) || []);
+
+  // Función para eliminar un producto
+  const handleDelete = (productId) => {
+    const updatedFavorites = favorites.filter(product => product.id !== productId);
+    setFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites)); // Actualizamos localStorage
+  };
 
   return (
     <div>
       <div className="product-list">
         {favorites.length > 0 ? (
-          favorites.map((product, index) => (
+          favorites.map((product) => (
             <React.Fragment key={product.id}>
               <div className="product-wrapper">
                 <ImageContainer image={product.image} />
@@ -19,8 +27,14 @@ const Favorites = () => {
                   name={product.name}
                   rating={product.rating}
                 />
+                {/* Botón de papelera con clase delete-button */}
+                <button 
+                  className="delete-button" 
+                  onClick={() => handleDelete(product.id)}
+                >
+                  <FaTrash size={20} /> {/* Ícono de la papelera */}
+                </button>
               </div>
-              {index < favorites.length - 1 && <div className="separator"></div>}
             </React.Fragment>
           ))
         ) : (
