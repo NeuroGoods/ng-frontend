f38d863c9e569c9fcb33e3be2aa994f646e9b2d4
marianamarinflor622 <marianamarinflor@gmail.com>
2025-03-20 20:39:44 +0100

    add components from

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Footer from '../../components/ui/Footer/Footer'
+import CategoryNav from '../../components/ui/CategoryNav/CategoryNav'
+import Input from '../../components/ui/Input/Input'
+import ImageContainer from '../
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <> 
+   
+    <Input/>
+    <ImageContainer/>
+    <CategoryNav/>
+    <Footer/></>
   )
 }
 
