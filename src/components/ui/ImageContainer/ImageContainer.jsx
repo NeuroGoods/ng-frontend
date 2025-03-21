de2d8c821ba16433d2a1b411353460f1a2234041
srlsrx <nicolasin97@gmail.com>
2025-03-21 06:41:07 +0100

    fix prop

--- src/components/ui/ImageContainer/ImageContainer.jsx
+++ src/components/ui/ImageContainer/ImageContainer.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styles from "./ImageContainer.module.css";
 
-function ImageContainer(imgPath) {
+function ImageContainer({imgPath}) {
   return <section className={styles.container}>
     <img style={{height:"100%", width:"100%"}} src={imgPath}/>
   </section>;
