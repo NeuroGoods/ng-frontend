77db7a3639f1b0c9f6ba80046ace37be0745e1f0
srlsrx <nicolasin97@gmail.com>
2025-03-21 02:12:33 +0100

    fix some things

--- src/components/ui/ImageContainer/ImageContainer.jsx
+++ src/components/ui/ImageContainer/ImageContainer.jsx
@@ -3,7 +3,7 @@ import styles from "./ImageContainer.module.css";
 
 function ImageContainer() {
   return <section className={styles.container}>
-    <img style={{height:"350px", width:"100%"}} src="https://www.dzoom.org.es/wp-content/uploads/2022/03/mis-fotografias-paisaje-favoritas-07-810x540.jpg"/>
+    <img style={{height:"100%", width:"100%"}} src="https://www.dzoom.org.es/wp-content/uploads/2022/03/mis-fotografias-paisaje-favoritas-07-810x540.jpg"/>
   </section>;
 }
 
