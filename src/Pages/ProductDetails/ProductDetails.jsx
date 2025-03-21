fd4844d810e5ee54e6a845f48faf75159837dd42
srlsrx <nicolasin97@gmail.com>
2025-03-21 01:58:36 +0100

    fix product details page

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,37 +1,36 @@
-import Button from "../../components/ui/Button/Button"
-import styles from "./ProductDetails.module.css"
+import Button from "../../components/ui/Button/Button";
+import styles from "./ProductDetails.module.css";
 import ImageContainer from "../../components/ui/ImageContainer/ImageContainer";
 import ContainerProductDetail from "../../components/ui/ContainerProductDetail/ContainerProductDetail";
 
-
 const ProductDetailsPage = () => {
   return (
-    <>
-    <ImageContainer />
-    <ContainerProductDetail />
-    {/* Contador de cantidad y tiempo de entrega */}
-    <div className={styles.quantityContainer}>
+    <div className={styles.pageContainer}>
+      <ImageContainer />
+
+      <div className={styles.card}>
+        <ContainerProductDetail />
+        <div className={styles.quantityContainer}>
           <button className={styles.quantityButton}>-</button>
           <span className={styles.quantity}>2</span>
           <button className={styles.quantityButton}>+</button>
           <span className={styles.time}>⏳ 5 Días</span>
         </div>
+
         
-        {/* Descripción */}
         <div className={styles.description}>
           <h3>Descripción</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim eget amet viverra eget fames rhoncus.
           </p>
-        <div>
-        <Button variation="light" text = "contactar"/>
-        <Button variation="primary" text = "comprar"/>
+          <div className={styles.buttonContainer}>
+            <Button variation="light" text="Contactar" />
+            <Button variation="primary" text="Comprar" />
+          </div>
         </div>
-        </div>
-    </>
+      </div>
+    </div>
   );
-}
+};
 
-
-  
 export default ProductDetailsPage;
--- src/components/ui/ImageContainer/ImageContainer.jsx
+++ src/components/ui/ImageContainer/ImageContainer.jsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import styles from "./ImageContainer.module.css";
 
 function ImageContainer() {
-  return <section className={styles.container}></section>;
+  return <section className={styles.container}>
+    <img style={{height:"350px", width:"100%"}} src="https://www.dzoom.org.es/wp-content/uploads/2022/03/mis-fotografias-paisaje-favoritas-07-810x540.jpg"/>
+  </section>;
 }
 
 export default ImageContainer;
