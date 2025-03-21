6c87c64db8472d3d073b6dfe4b8e70a7b2e4e552
srlsrx <nicolasin97@gmail.com>
2025-03-21 02:41:22 +0100

    fix some things

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import Button from "../../components/ui/Button/Button";
 import styles from "./ProductDetails.module.css";
 import ImageContainer from "../../components/ui/ImageContainer/ImageContainer";
 import ContainerProductDetail from "../../components/ui/ContainerProductDetail/ContainerProductDetail";
 
 const ProductDetailsPage = () => {
+  const [quantity, setQuantity] = useState(1); // Estado para el contador
+
+  const increaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity((prev) => prev - 1);
+    }
+  };
+
   return (
     <div className={styles.pageContainer}>
       <ImageContainer />
@@ -11,13 +24,12 @@ const ProductDetailsPage = () => {
       <div className={styles.card}>
         <ContainerProductDetail />
         <div className={styles.quantityContainer}>
-          <button className={styles.quantityButton}>-</button>
-          <span className={styles.quantity}>2</span>
-          <button className={styles.quantityButton}>+</button>
+          <button className={styles.quantityButton} onClick={decreaseQuantity}>-</button>
+          <span className={styles.quantity}>{quantity}</span>
+          <button className={styles.quantityButton} onClick={increaseQuantity}>+</button>
           <span className={styles.time}>⏳ 5 Días</span>
         </div>
 
-        
         <div className={styles.description}>
           <h3>Descripción</h3>
           <p>
