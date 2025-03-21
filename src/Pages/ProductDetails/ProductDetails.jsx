b1c0679c17fdb795bc486d2b50972a62531a2df6
Paola Morales <dissenyaspasia@gmail.com>
2025-03-21 01:24:22 +0100

    changes-product-details-page

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,36 +1,47 @@
-import Button from "../../components/ui/Button/Button"
-import styles from "./ProductDetails.module.css"
+import Button from "../../components/ui/Button/Button";
+import styles from "./ProductDetails.module.css";
 import ImageContainer from "../../components/ui/ImageContainer/ImageContainer";
 import ContainerProductDetail from "../../components/ui/ContainerProductDetail/ContainerProductDetail";
+import Navbar from "../../components/ui/Navbar/Navbar";
 
 
 const ProductDetailsPage = () => {
   return (
-    <>
-    <ImageContainer />
-    <ContainerProductDetail />
-    {/* Contador de cantidad y tiempo de entrega */}
-    <div className={styles.quantityContainer}>
+    <div className={styles.productPage}>
+      {/* Imagen del producto */}
+      <ImageContainer />
+
+      {/* Detalles del producto */}
+      <ContainerProductDetail />
+
+      {/* Contador de cantidad y tiempo de entrega */}
+      <div className={styles.detailsContainer}>
+        <div className={styles.quantityContainer}>
           <button className={styles.quantityButton}>-</button>
           <span className={styles.quantity}>2</span>
           <button className={styles.quantityButton}>+</button>
-          <span className={styles.time}>⏳ 5 Días</span>
         </div>
-        
-        {/* Descripción */}
-        <div className={styles.description}>
-          <h3>Descripción</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim eget amet viverra eget fames rhoncus.
-          </p>
-        <div>
-        <Button variation="light" text = "contactar"/>
-        <Button variation="primary" text = "comprar"/>
-        </div>
-  
-  
-    </>
-  )
-}
-  
+        <span className={styles.time}>⏳ 5 Días</span>
+      </div>
+
+      {/* Descripción */}
+      <div className={styles.description}>
+        <h3>Descripción</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim
+          eget amet viverra eget fames rhoncus. Eget enim venenatis enim porta
+          egestas malesuada et. Consequat mauris lacus euismod montes.
+        </p>
+      </div>
+
+      {/* Botones de acción */}
+      <div>
+      <Button variation="light" text = "contactar"/>
+      <Button variation="primary" text = "comprar"/>
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
 export default ProductDetailsPage;
